965212cc6bfa8d779358cc4fd11572e7d73e7130
Alemayehu <alemayehudabi606@gmail.com>
2025-09-29 22:06:09 +0300

    fix: change llm form groq to gimini

--- src/rag_pipeline.py
+++ src/rag_pipeline.py
@@ -1,10 +1,21 @@
 from langchain.chains import RetrievalQA
-from langchain_groq import ChatGroq
 from retriever import get_retriever
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+import os
+
+load_dotenv()
 
 def create_rag_pipeline():
     retriever = get_retriever()
-    llm = ChatGroq(model="llama3-8b-8192", temperature=0)
+    groq_api_key = os.getenv("GOOGLE_API_KEY")
+    if not groq_api_key:
+        raise ValueError(
+            "❌ GOOGLE_API_KEY is not set. Please add it to your .env file!"
+        )
+        
+    llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash",
+        temperature=0.5)
     qa_chain = RetrievalQA.from_chain_type(
         llm=llm,
         retriever=retriever,
